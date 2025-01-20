51d3703ab4edb02981b6db7cf49b33b2d7a7cb86
นายโอ๊ต ศรีไชยนิวาศน์ <oat.soat@365.pnru.ac.th>
2025-01-20 17:44:32 +0700

    add CollectCoverage

--- starter/obj/Debug/net8.0/starter.AssemblyInfo.cs
+++ starter/obj/Debug/net8.0/starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91f203503db67c8288dbb4bd977d4f9ca82d6a70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+777585adec9c32a8074d27a17901fe3bc3990fbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
